6eb3b92f42bf459cbd53b2b766b2b0b30ffb4dd3
APDALLE12 <cabdala996712@gmail.com>
2026-09-20 18:13:40 +0300

    aded class-apiconfigration on file class.py

--- class-python/class.py
+++ class-python/class.py
@@ -1,11 +1,17 @@
-class dog :
-    def __init__(self,name,breed):
-        self.name = name
-        self.breed = breed
-dog1 = dog("Buddy", "Golden Retriever")
-print(f"dog name :{dog1.name}")
+class APIConfig:
+    def __init__(self,api_key,model ="gpt-3.5-turbo",max_tokens=100):
+        self.api_key = api_key
+        self.model = model
+        self.max_tokens = max_tokens
+        self.base_url = "https://api.openai.com/v1"
+# create different configuration
+#using positional for required arg ,name  for optional
+dev_config = APIConfig("sky_dev_key",max_tokens=200) 
 
-class cat :
-    def __init__(self,name,color):
-        self.name = name
-        self.color = color
+# using all named arguments (clearest)
+prod_config = APIConfig(api_key="sky_prod_key",model="gpt-4",max_tokens=500)
+
+# access for configuration
+print(f"Dev Config: {dev_config.model}")
+print(f"Prod Config: {prod_config.model}")
+print(f"Prod Config Max Tokens: {prod_config.max_tokens}")
