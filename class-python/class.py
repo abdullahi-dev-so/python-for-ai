a4efd29231bfc7bece47af2ff9d49258168a3c0e
APDALLE12 <cabdala996712@gmail.com>
2026-09-20 17:58:32 +0300

    added folder class

---
+++ class-python/class.py
@@ -0,0 +1,11 @@
+class dog :
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
+dog1 = dog("Buddy", "Golden Retriever")
+print(f"dog name :{dog1.name}")
+
+class cat :
+    def __init__(self,name,color):
+        self.name = name
+        self.color = color
